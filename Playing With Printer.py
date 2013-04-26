37dc2ebc73efde8b0cd151fb38928f4d76e62bb0
Pete Mawle <semajetep@me.com>
2013-04-26 13:54:56 +0100

    Just Starting
    :-)

---
+++ Playing With Printer.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+"""
+Insert custom messages into HP printer displays
+
+Works for most network-enabled HP printers.
+"""
+
+import socket
+import sys
+import os.path
+
+# Default configuration
+host = '10.1.54.6'
+port = 9100 #9100 is the default port.
+
+sock = socket.socket()
+try:
+    sock.connect((host, port))
+except socket.error, e:
+    sys.exit('Connection error %s: %s.' % (e[0], e[1]))
+
+print 'Connected'
+
+message ="Ready"
+print 'Setting ready message to "%s" on %s' % (message, host)
+command = "\x1B%%-12345X@PJL RDYMSG DISPLAY = \"%s\"\r\n\x1B%%-12345X\r\n" % message
+sock.sendall(command)
+
